3be80e43278a21ac5bef9801a41031d93ead2f4d
Ats0c_ <173511395+At30c@users.noreply.github.com>
2025-12-27 01:10:28 +0000

    Changing KSU to KSU Next

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Installing dependencies..."
+sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y install bc cpio repo git-core gnupg flex bison build-essential zip curl zlib1g-dev libc6-dev-i386 x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig
+
 abort()
 {
     cd -
